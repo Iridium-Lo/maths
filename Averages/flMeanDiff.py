5e78580b2bbf8e69bac32ff5df8587acc492a441
Iridium-Lo <80277972+Iridium-Lo@users.noreply.github.com>
2021-06-08 11:05:31 +0100

    Update and rename differenceMean.py to flMeanDiff.py

--- Averages/flMeanDiff.py
+++ Averages/flMeanDiff.py
@@ -3,10 +3,9 @@ nums = [3, 1, 2, 5, 1, 5, -7, 9, -8, -3, 3]
 diff_list = []
 
 for i in range(len(nums) - 1):
-    diff = abs(nums[0] - nums[1])
+    diff = abs(nums[i] - nums[i+1])
     diff_list.append(diff)
-    nums.pop(0)
 
-mean = sum(diff_list)/len(diff_list)
+mean = sum(diff_list) / len(diff_list)
 
 print(round(mean, 1))
