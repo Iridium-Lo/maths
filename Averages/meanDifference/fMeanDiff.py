e3b4a82c7596290c3af803e116b5d639e36a1830
Iridium-Lo <80277972+Iridium-Lo@users.noreply.github.com>
2021-06-09 18:53:38 +0100

    Update fMeanDiff.py

--- Averages/meanDifference/fMeanDiff.py
+++ Averages/meanDifference/fMeanDiff.py
@@ -4,10 +4,10 @@ def diff(nums: List[int]) -> List[int]:
     return [abs(nums[i] - nums[i + 1]) for i in range(len(nums) - 1)]
 
 def mean(lst: List[int]) -> float:
-    return sum(lst) / len(lst)
+    return round(sum(lst) / len(lst), 1)
 
 def mean_diff(nums: List[int], diff) -> None:
-    return round(mean(diff(nums)), 1)
+    return mean(diff(nums))
 
 
 nums = [3, 1, 2, 5, 1, 5, -7, 9, -8, -3, 3]
